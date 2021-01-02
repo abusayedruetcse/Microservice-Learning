958912798d980626556e62f4d8952c385b1cb5cb
Abu Sayed Ahmad <sayed.ahmad@technovista.com.bd>
2021-01-02 23:53:09 +0600

    3 EF packages added, connectionString added in DbContext, add-migration & update-database -verbose run done.

--- SimpleMicroservice/UserService/Database/DatabaseContext.cs
+++ SimpleMicroservice/UserService/Database/DatabaseContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserService.Database.Entities;
 
 namespace UserService.Database
 {
-    public class DatabaseContext
+    public class DatabaseContext: DbContext
     {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=DESKTOP-9STLETQ\MSSQLEXPRESS2019; initial catalog=UserMicroservice;
+            persist security info=True; user id=sa; password=sa1234;");
+        }
     }
 }
